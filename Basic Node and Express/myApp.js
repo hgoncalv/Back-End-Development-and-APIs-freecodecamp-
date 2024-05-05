e2f6f648f9c81ec4f3091f553c253de6d2278ede
hgoncalv <iceburn23@gmail.com>
2024-05-05 16:37:37 +0100

    Chain Middleware to Create a Time Server

--- Basic Node and Express/myApp.js
+++ Basic Node and Express/myApp.js
@@ -27,4 +27,15 @@ app.get("/json", (req, res) => {
   res.send({ message });
 });
 
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.send({ time: req.time });
+  }
+);
+
 module.exports = app;
