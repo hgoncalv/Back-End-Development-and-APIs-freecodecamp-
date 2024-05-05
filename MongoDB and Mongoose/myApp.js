e1391c4ac2ed4132895d6bb2746831eba6708b91
hgoncalv <iceburn23@gmail.com>
2024-05-05 21:35:07 +0100

    Delete One Document Using model.findByIdAndRemove

--- MongoDB and Mongoose/myApp.js
+++ MongoDB and Mongoose/myApp.js
@@ -89,7 +89,12 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findOneAndRemove({ _id: personId }, (err, data) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
