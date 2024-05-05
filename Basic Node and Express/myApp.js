b8aeba914388b5c6caf2f20ad643f0217e8a53c9
hgoncalv <iceburn23@gmail.com>
2024-05-05 17:04:19 +0100

    Get Query Parameter Input from the Client

--- Basic Node and Express/myApp.js
+++ Basic Node and Express/myApp.js
@@ -42,4 +42,13 @@ app.get("/:word/echo", (req, res) => {
   res.send({ echo: req.params.word });
 });
 
+app
+  .route("/name")
+  .get((req, res) => {
+    res.send({ name: `${req.query.first} ${req.query.last}` });
+  })
+  .post((req, res) => {
+    //
+  });
+
 module.exports = app;
