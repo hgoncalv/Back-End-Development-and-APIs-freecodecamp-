2d8c7628363912f619cf5d7fa6b95611751eaa13
hgoncalv <iceburn23@gmail.com>
2024-05-05 23:24:47 +0100

    Chain Search Query Helpers to Narrow Search Results

--- MongoDB and Mongoose/myApp.js
+++ MongoDB and Mongoose/myApp.js
@@ -109,8 +109,17 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => {
+      if (err) {
+        return done(err);
+      } else {
+        done(null, data);
+      }
+    });
 };
 
 /** **Well Done !!**
